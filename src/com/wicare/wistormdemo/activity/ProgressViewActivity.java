50451d06fdfe1d3ea08bc9938598772499fe24d9
Administrator <Administrator@PC--20150902QLW>
2016-02-24 17:20:47 +0800

    WCircleProView  demo功能添加

--- src/com/wicare/wistormdemo/activity/ProgressViewActivity.java
+++ src/com/wicare/wistormdemo/activity/ProgressViewActivity.java
@@ -8,6 +8,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -18,24 +20,54 @@ public class ProgressViewActivity extends Activity {
 	
 	private WArcProView arcView;//弧形进度UI
 	private WCircleProView circleView;//圆形进度UI
+	private WCircleProView circleViewPointRun;
+	
 	private SeekBar seekBar;
 	private TextView tvArcProgress;
 	private TextView tvCircleProgress;
+	private TextView tvCircleProgressRunning;
+	
 	private String tv;
+	
+	boolean pointRunning = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.progress_view);
-	       
+		//内环形也动态绘制进度
         arcView = (WArcProView)findViewById(R.id.v_arc);
-//        arcView.enableInsideScaleRing(true);//内环形也动态绘制进度
-        
+//        arcView.enableInsideScaleRing(true);
+      //内环形也动态绘制进度
         circleView = (WCircleProView)findViewById(R.id.v_circle);
-        circleView.enableInsideScaleRing(true);//内环形也动态绘制进度
+        circleView.enableInsideScaleRing(true);
+      //只有外层圆环上的点转动
+        circleViewPointRun = (WCircleProView)findViewById(R.id.v_circle_running);
+        circleViewPointRun.enableScaleRingPointRun(true);
         
         tvArcProgress = (TextView)findViewById(R.id.tv_arc);
         tvCircleProgress = (TextView)findViewById(R.id.tv_circle);
+        tvCircleProgressRunning = (TextView)findViewById(R.id.tv_circle_running);
+        
+        tvCircleProgressRunning.setText("STOP");
+        
+        
+        tvCircleProgressRunning.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				pointRunning = !pointRunning;
+				if(pointRunning){
+					circleViewPointRun.enableScaleRingPointRun(true);
+					tvCircleProgressRunning.setText("STOP");
+				}else{
+					circleViewPointRun.enableScaleRingPointRun(false);
+					tvCircleProgressRunning.setText("GO");
+				}
+				
+			}
+		});
         
         seekBar = (SeekBar)findViewById(R.id.seek_bar) ;
         seekBar.setMax(100);
